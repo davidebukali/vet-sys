3553c121caa12b4025f15cb9ea905da4fbbe1e66
David <debukali@gmail.com>
2025-09-13 16:00:44 +0300

    Add settings custom page

---
+++ app/Filament/Pages/Settings.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Widgets\PatientTypeOverview;
+use Filament\Pages\Page;
+use Filament\Actions\Action;
+
+class Settings extends Page
+{
+    protected static ?string $title = 'App Settings'; // Same as heading
+
+    protected ?string $subheading = 'Update application settings here';
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.settings';
+
+    public static function canAccess(): bool
+    {
+        return true;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('dashboard')
+                ->url(route('filament.admin.pages.dashboard')),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PatientTypeOverview::make([
+                'cats' => 1,
+            ]),
+        ];
+    }
+
+    public function getHeaderWidgetsColumns(): int | array
+    {
+        return 1;
+    }
+}
