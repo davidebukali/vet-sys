8a6e26a409fd6c800d3b8fc29d2c476c6f4a783a
David <debukali@gmail.com>
2025-09-14 20:14:36 +0300

    Add settings page to cluster

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Pages;
 
 use App\Filament\Widgets\PatientTypeOverview;
+use App\Filament\Clusters\SettingsCluster;
 use Filament\Pages\Page;
 use Filament\Actions\Action;
 
 class Settings extends Page
 {
+    protected static ?string $cluster = SettingsCluster::class;
+
     protected static ?string $title = 'App Settings'; // Same as heading
 
     protected ?string $subheading = 'Update application settings here';
