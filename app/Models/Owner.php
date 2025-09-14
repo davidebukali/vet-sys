729f6924788d037f333e8dfc5edc7889b1966ab6
David <debukali@gmail.com>
2025-09-14 22:28:06 +0300

    Add owner test factory

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Database\Factories\OwnerTestFactory;
 use App\Models\Patient;
 
 class Owner extends Model
 {
+    use HasFactory;
+
+    /**
+     * Create a new factory instance for the model.
+     */
+    protected static function newFactory(): OwnerTestFactory
+    {
+        return OwnerTestFactory::new();
+    }
+
     public function patients(): HasMany
     {
         return $this->hasMany(Patient::class);
---
+++ database/factories/OwnerTestFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Owner;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Owner>
+ */
+class OwnerTestFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Owner::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+        ];
+    }
+}
