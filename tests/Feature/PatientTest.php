1b4b722fb2a0379778ec61a6e7188d3dfe9bb98c
David <debukali@gmail.com>
2025-09-15 17:25:21 +0300

    Add patient resource tests

--- tests/Feature/PatientTest.php
+++ tests/Feature/PatientTest.php
@@ -11,24 +11,50 @@ use App\Models\Owner;
 use App\Models\User;
 use App\Models\Patient;
 
-class PatientTest extends TestCase
-{
-    use RefreshDatabase;
+uses(RefreshDatabase::class);
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->actingAs(User::factory()->create());
-    }
+it('can render patient list page', function () {
+    $this->get(PatientResource::getUrl('index'))->assertSuccessful();
+});
 
-    /**
-     * Test patient list.
-     */
-    public function test_it_loads_patient_list(): void
-    {;
-        $owner = Owner::factory()->create();
-        $patients = Patient::factory()->count(10)->for($owner)->create();
-        Livewire::test(PatientResource\Pages\ListPatients::class)
-            ->assertCanSeeTableRecords($patients);
-    }
-}
+it('can view patient table records', function () {
+    $owner = Owner::factory()->create();
+    $patients = Patient::factory()->count(10)->for($owner)->create();
+    Livewire::test(PatientResource\Pages\ListPatients::class)
+        ->assertCanSeeTableRecords($patients);
+});
+
+it('can view create page for patients', function () {
+    $this->get(PatientResource::getUrl('create'))->assertSuccessful();
+});
+
+it('can create a patient', function () {
+    $owner = Owner::factory()->create();
+    $newData = Patient::factory()->for($owner)->create();
+
+    Livewire::test(PatientResource\Pages\CreatePatient::class)
+        ->fillForm([
+            'owner_id' => $newData->owner->getKey(),
+            'name' => $newData->name,
+            'type' => $newData->type,
+            'date_of_birth' => $newData->date_of_birth,
+        ])
+        ->call('create')
+        ->assertHasNoFormErrors();
+
+    $this->assertDatabaseHas(Patient::class, [
+        'owner_id' => $newData->owner->getKey(),
+        'name' => $newData->name,
+        'type' => $newData->type,
+        'date_of_birth' => $newData->date_of_birth,
+    ]);
+});
+
+it('can validate patient input', function () {
+    Livewire::test(PatientResource\Pages\CreatePatient::class)
+        ->fillForm([
+            'name' => null,
+        ])
+        ->call('create')
+        ->assertHasFormErrors(['name' => 'required']);
+});
